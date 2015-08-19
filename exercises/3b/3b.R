7c0fe14836eb34eda5f410901dbcfadde8737779
Alexander Junge <ajunge@rth.dk>
2015-08-19 09:18:33 +0200

    adjust to new directory structure

--- exercises/3b/3b.R
+++ exercises/3b/3b.R
@@ -1,13 +1,14 @@
+# Exercises for Lecture 3b - The Support Vector Machine and Convex Optimization
+
+## Exercise 4
 library(e1071)
 library(R.matlab)
 
-acs.mat <- readMat('Lecture 3 Classifiers/M3b/M3b/ACS.mat')
+acs.mat <- readMat('../../lectures/Lecture 3 Classifiers/M3b/M3b/ACS.mat')
 x.truncated <- acs.mat$X.truncated
 x.scaled <- data.frame(scale(x.truncated))
 y <- as.factor(acs.mat$y)
 data.df <- cbind(x.scaled, y)
-
-#svm.model <- svm(y~., data=data.df, cost=1, cross=10)
 
 tuned.svm <- tune.svm(y~., data=data.df, cost=c(0.1, 1, 10, 100, 1000),
                       gamma=c(0.5, 1, 2, 3, 4),
